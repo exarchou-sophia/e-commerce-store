9c3711c97902c820836603d0fc750b15b965102a
Manuel <manuel.penner@mein.gmx>
2024-09-30 20:15:45 +0200

    added dark theme in html and daysiUi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
-    return <div>Home</div>;
+    const [isdark, setIsdark] = useState(
+      JSON.parse(localStorage.getItem('isdark'))
+    );
+    useEffect(() => {
+      localStorage.setItem('isdark', JSON.stringify(isdark));
+    }, [isdark]);
+    return (
+        <>
+            <div>
+<input
+  type="checkbox"
+  value="synthwave"
+                    className="toggle theme-controller"
+  checked={isdark}
+  onChange={() => setIsdark(!isdark)}
+  />
+            </div>
+            <div>Home</div>
+        </>
+    );
 };
 
 export default Home;
