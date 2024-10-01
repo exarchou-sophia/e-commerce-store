d7ad587b9b336d7948d25bedcd1351c0d931f06e
Manuel <manuel.penner@mein.gmx>
2024-10-01 10:54:05 +0200

    moved filter component to Filter.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,8 @@
-// import Filter from "../components/Filter";
+import Filter from "../components/Filter";
 import { ProductCard } from "../components/ProductCard";
 import FetchApi from "../Utils/FetchApi";
 import React, { useState } from "react";
 
-const products2 = [
-    { id: 1, title: "Men's T-Shirt", price: "$20", category: "men" },
-    { id: 2, title: "Women's Dress", price: "$40", category: "women" },
-    { id: 3, title: "Smartphone", price: "$300", category: "tech" },
-    { id: 4, title: "Laptop", price: "$1000", category: "tech" },
-    { id: 5, title: "Men's Jeans", price: "$50", category: "men" },
-    { id: 6, title: "Women's Skirt", price: "$30", category: "women" },
-];
 const products = JSON.parse(localStorage.getItem("productData")) || [];
 const Home = () => {
     const [filter, setFilter] = useState("all");
@@ -22,22 +14,7 @@ const Home = () => {
         <>
             <div>
                 <FetchApi />
-                <div className="filters flex justify-around mx-4 p-4">
-                    <button onClick={() => setFilter("all")}>All</button>
-                    <button onClick={() => setFilter("men's clothing")}>
-                        Men's Clothes
-                    </button>
-                    <button onClick={() => setFilter("jewelery")}>
-                        Jewelery
-                    </button>
-                    <button onClick={() => setFilter("electronics")}>
-                        Tech
-                    </button>
-                    <button onClick={() => setFilter("women's clothing")}>
-                        Women's Clothing
-                    </button>
-                </div>
-
+                <Filter />
                 <div className="product-grid grid grid-cols-4 gap-4">
                     {filteredProducts.map(item => (
                         <ProductCard key={item.id} {...item}/>
